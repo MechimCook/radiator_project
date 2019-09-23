f2699bc7230018690415e1a0447ef12c8a9a0b67
Mechim <mechim@getlua.com>
2019-09-23 13:38:09 -0400

    Parse file

--- app/lib/app.ex
+++ app/lib/app.ex
@@ -4,15 +4,23 @@ defmodule App do
   """
 
   @doc """
-  Hello world.
+  parse update.
 
   ## Examples
 
-      iex> App.hello()
-      :world
+      iex> App.parse_update(file_name)
+      [file_line1,file_line2,est]
 
   """
-  def hello do
-    :world
+  def parse_update(file_name) do
+    case File.read("../" <> file_name) do
+      {:ok, content} ->
+        content
+        |> String.split("\r\n", trim: true)
+
+      {:error, error} ->
+        error <>
+          "Please make sure the file is in the same file as the application and that you are giving the file's name"
+    end
   end
 end
