671a8845f4a55ff77822b859da04233e313a7d9a
Mechim <mechim@getlua.com>
2019-09-24 11:27:46 -0400

    Add dependencies

--- app/mix.exs
+++ app/mix.exs
@@ -21,6 +21,8 @@ defmodule App.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 1.5"},
+      {:binary, "~> 0.0.5"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
